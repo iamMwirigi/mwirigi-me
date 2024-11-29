6f66b5b7e1c4695302a29ead75e7d2d709d4d7a9
mwirigi <mwirigimburugu81@gmail.com>
2024-11-29 03:04:56 +0300

    few corrections

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,12 +7,13 @@ const About = () => {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         {/* Text section */}
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Mwirigi.
-            <span className="block sm:inline">I'm a Software Developer</span>
-            <span className="block sm:inline">We lock-in here fam!</span>
-            <span className="block sm:inline">⭐⭐⭐⭐⭐</span>
-          </h1>
+        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+          Hi, I'm Mwirigi.
+          <span className="block sm:inline mt-4">I'm a Software Developer.</span>
+          <span className="block sm:inline mt-4">Clocked-in.</span>
+          <span className="block sm:inline mt-4">⭐⭐⭐⭐⭐</span>
+        </h1>
+
 
           <p className="mb-8 leading-relaxed">
             A self-motivated, passionate, enthusiastic and certified software engineer from Kenya. I
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -37,20 +37,67 @@ export default function Contact() {
   return (
     <section id="contact" className="relative">
       <div className="container w-[500px] px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-        {/* Map Container */}
-        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex flex-col justify-between relative">
-          <iframe
-            width="100%"
-            height="100%"
-            title="map"
-            className="absolute inset-0"
-            frameBorder={0}
-            marginHeight={0}
-            marginWidth={0}
-            style={{ filter: "opacity(0.7)" }}
-            src="https://www.google.com/maps/embed/v1/place?q=Uthiru,+Nairobi&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
-          />
-        </div>
+        
+        {/* Contact Form */}
+        <form
+          onSubmit={handleSubmit}
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+          onKeyDown={handleKeyDown} // Add this line
+        >
+          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+            Hire Me
+          </h2>
+          <div className="relative mb-4">
+            
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setName(e.target.value)}
+              value={name} // Add value for controlled input
+              placeholder="Enter your name or company name"  // Placeholder text
+              required
+            />
+            <p className="text-xs text-gray-400 mt-2">Please enter your full name or company name.</p> {/* Helper text */}
+          </div>
+
+          <div className="relative mb-4">
+            
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email} // Add value for controlled input
+              placeholder="Enter your email address"  // Placeholder text
+              required
+            />
+            <p className="text-xs text-gray-400 mt-2">Enter a valid email address so we can get in touch.</p> {/* Helper text */}
+          </div>
+
+          <div className="relative mb-4">
+          
+            <textarea
+              id="message"
+              name="message"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setMessage(e.target.value)}
+              value={message} // Add value for controlled input
+              placeholder="Write your message here..."  // Placeholder text
+              required
+            />
+            <p className="text-xs text-gray-400 mt-2">Please provide a detailed message for your inquiry.</p> {/* Helper text */}
+          </div>
+
+          <button
+            type="submit"
+            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          >
+            Submit
+          </button>
+        </form>
 
         {/* Contact Info Container */}
         <div className="lg:w-1/4 md:w-1/3 bg-gray-900 rounded-lg overflow-hidden p-4 flex flex-col justify-end h-full">
@@ -61,6 +108,27 @@ export default function Contact() {
               </h2>
               <p className="mt-1">Uthiru, Nairobi, Kenya.</p>
             </div>
+
+            {/* Map Container - Placed between Address and Email */}
+            <div className="px-4 mb-2">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                MAP
+              </h2>
+              <div className="w-full h-40 mb-2 relative">
+                <iframe
+                  width="100%"
+                  height="100%"
+                  title="map"
+                  className="absolute inset-0"
+                  frameBorder={0}
+                  marginHeight={0}
+                  marginWidth={0}
+                  style={{ filter: "opacity(0.7)" }}
+                  src="https://www.google.com/maps/embed/v1/place?q=Uthiru,+Nairobi&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+                />
+              </div>
+            </div>
+
             <div className="px-4 mb-2">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
@@ -127,67 +195,6 @@ export default function Contact() {
             </div>
           </div>
         </div>
-
-        {/* Contact Form */}
-        <form
-          onSubmit={handleSubmit}
-          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
-          onKeyDown={handleKeyDown} // Add this line
-        >
-          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            Hire Me
-          </h2>
-          <div className="relative mb-4">
-            
-            <input
-              type="text"
-              id="name"
-              name="name"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setName(e.target.value)}
-              value={name} // Add value for controlled input
-              placeholder="Enter your name or company name"  // Placeholder text
-              required
-            />
-            <p className="text-xs text-gray-400 mt-2">Please enter your full name or company name.</p> {/* Helper text */}
-          </div>
-
-          <div className="relative mb-4">
-            
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setEmail(e.target.value)}
-              value={email} // Add value for controlled input
-              placeholder="Enter your email address"  // Placeholder text
-              required
-            />
-            <p className="text-xs text-gray-400 mt-2">Enter a valid email address so we can get in touch.</p> {/* Helper text */}
-          </div>
-
-          <div className="relative mb-4">
-          
-            <textarea
-              id="message"
-              name="message"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setMessage(e.target.value)}
-              value={message} // Add value for controlled input
-              placeholder="Write your message here..."  // Placeholder text
-              required
-            />
-            <p className="text-xs text-gray-400 mt-2">Please provide a detailed message for your inquiry.</p> {/* Helper text */}
-          </div>
-
-          <button
-            type="submit"
-            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-          >
-            Submit
-          </button>
-        </form>
       </div>
     </section>
   );
